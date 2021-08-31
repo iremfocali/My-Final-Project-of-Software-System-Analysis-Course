c21f32ec38b2f8ad70169769f5bf08c0c8564221
iremfocali <irem.focali@hotmail.com>
2021-08-31 14:27:13 +0300

    Favorite Books Desktop Application
    My Desktop Application (with GUI) developed with Java programming language

---
+++ FavoriteBooks/src/favoritebooks/FavoriteBooks.java
@@ -0,0 +1,18 @@
+package favoritebooks;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import javax.swing.JFrame;
+
+public class FavoriteBooks {
+
+        public static void main(String[] args) {
+        
+        LoginFrame loginframe = new LoginFrame();
+        loginframe.setSize(600,600);
+        loginframe.setVisible(true);
+        loginframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    }
+}
